85d2cb5cf6e6740a8fa777f019c176f006204970
Tanmay41 <tanmayror19@gmail.com>
2023-12-26 13:46:18 +0530

    Shows Hello in your language based on you location

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
+import axios from "axios";
+import { log } from "console";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  const result = await axios.get(`https://ipapi.co/json/`);
+  const countryCode = result.data.country_code;
+  const Hello = await axios.get(
+    `https://hellosalut.stefanbohacek.dev/?cc=${countryCode}`
+  );
+  const HTMLHello = Hello.data.hello;
+  res.render("index.ejs", {
+    data: { Hello: HTMLHello },
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
