e854c1b92e256a759c9c390463ea856a6eb2a489
Tanmay41 <tanmayror19@gmail.com>
2023-12-26 12:37:56 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
